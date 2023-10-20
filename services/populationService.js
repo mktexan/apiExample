8aaa47766d8b73f47c5bdf1d5397d28c9edb08a3
mktexan <mkcoarng@gmail.com>
2023-10-20 12:07:24 -0600

    refactor controller

--- services/populationService.js
+++ services/populationService.js
@@ -2,10 +2,9 @@ const database = require('../db/population')
 
 const getPopulationData = (city, state) => {
     return new Promise((resolve, reject) => {
-        const stateExists = database[state] !== undefined
-        const cityExists = stateExists ? database[state][city] !== undefined : undefined
+        const stateAndCityExist = database[state]?.[city]
 
-        if (!stateExists || !cityExists) return reject()
+        if (!stateAndCityExist) return reject()
 
         const population = { population: Number(database[state][city]) }
 
