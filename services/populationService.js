0a6ac437f42087674de1f08e548cb011c4a7aae4
mktexan <mkcoarng@gmail.com>
2023-10-22 20:06:45 -0600

    fixed naming for consistency

--- services/populationService.js
+++ services/populationService.js
@@ -1,12 +1,12 @@
-const database = require('../services/dbService')
+const dbService = require('../services/dbService')
 
 const getPopulationData = (city, state) => {
     return new Promise((resolve, reject) => {
-        const stateAndCityExist = database.populationData[state]?.[city]
+        const stateAndCityExist = dbService.populationData[state]?.[city]
 
         if (!stateAndCityExist) return reject()
 
-        const population = { population: Number(database.populationData[state][city]) }
+        const population = { population: Number(dbService.populationData[state][city]) }
 
         return resolve(population)
     })
@@ -14,13 +14,13 @@ const getPopulationData = (city, state) => {
 
 const modifyPopulationData = (city, state, populationData) => {
     return new Promise((resolve, reject) => {
-        const stateExists = database.populationData[state] !== undefined
-        const cityExists = stateExists ? database.populationData[state][city] !== undefined : undefined
+        const stateExists = dbService.populationData[state] !== undefined
+        const cityExists = stateExists ? dbService.populationData[state][city] !== undefined : undefined
         const isNewData = !stateExists || !cityExists
 
-        if (!stateExists) database.populationData[state] = {}
+        if (!stateExists) dbService.populationData[state] = {}
 
-        database.populationData[state][city] = Number(populationData)
+        dbService.populationData[state][city] = Number(populationData)
 
         return resolve(isNewData)
     })
